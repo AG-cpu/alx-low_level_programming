25f861efc6215bed11e243007dcfd1b6b4a0c886
AG-cpu <ahmadefatehy@gmail.com>
2022-04-10 05:48:53 -0700

    re10422

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,29 @@
 *
 * Return: Always 0 (Success)
 */
-	int main(void)
+
+int main(void)
 {
 	int n1, n2;
 
-for (n1 = 0; n1 <= 98; n1++)
-{
-for (n2 = n1 + 1; n2 <= 99; n2++)
+	for (n1 = 0; n1 <= 98; n1++)
 	{
-	putchar((n1 / 10) + '0');
-	putchar((n1 % 10) + '0');
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+		putchar((n1 / 10) + '0');
+		putchar((n1 % 10) + '0');
+		putchar(' ');
+		putchar((n2 / 10) + '0');
+		putchar((n2 % 10) + '0');
+		if (n1 == 98 && n2 == 99)
+		continue;
 
-	putchar(' ');
+		putchar(',');
+		putchar(' ');
+		}
+	}
 
-	putchar((n2 / 10) + '0');
-	putchar((n2 % 10) + '0');
-	putchar(',');
-	putchar(' ');
-}
-}
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
