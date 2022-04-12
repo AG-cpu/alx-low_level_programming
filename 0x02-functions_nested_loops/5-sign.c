26d1bdc74ee7e6e267ccd1f8a8e09d0b6b28ab9c
AG-cpu <ahmadefatehy@gmail.com>
2022-04-12 08:46:57 -0700

    n12422

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - to makr - + , for num.
+ * @n: the test opject variabl.
+ * Return: 1 , 0 , -1 as num sign.
+ */
+	int print_sign(int n)
+{
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+	else if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
