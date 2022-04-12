4dbeac13ce941696c57142c90f54b77c7c4abb63
AG-cpu <ahmadefatehy@gmail.com>
2022-04-12 07:10:12 -0700

    n12422

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - check the code
+ * print_alphabet - print_alph_etc
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+print_alphabet(void)
+
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	_putchar(letter);
 _putchar('\n');
