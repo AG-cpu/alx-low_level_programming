f1f17bebe99f5fbbfc2fb1fcf62d51e3b421f4ba
AG-cpu <ahmadefatehy@gmail.com>
2022-04-13 09:34:29 -0700

    re13422

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,8 @@
-#define MAIN_h
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +15,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+#endif /* MAIN_H */
