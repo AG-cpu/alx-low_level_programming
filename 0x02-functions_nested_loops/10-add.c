0dbf279de2e6414d52b0566adfe2237b2254112b
AG-cpu <ahmadefatehy@gmail.com>
2022-04-13 09:10:15 -0700

    re13422

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Addition of two numb.
+ * @n1: int to be added.
+ * @n2: 2int to be added.
+ * Return: addition.
+ */
+
+int add(int n1, int n2)
+
+{
+return (n1 + n2);
+}
+
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - from any numb input to 98,
+ * @n: The checking point.
+ */
+void print_to_98(int n)
+
+{
+if (n >= 98)
+{
+	while (n > 98)
+	{
+	printf("%d, ", n--);
+	printf("%d\n", n);
+	}
+}
+
+else
+{
+	while (n < 98)
+	{
+	printf("%d, ", n++);
+	printf("%d\n", n);
+	}
+}
+}
