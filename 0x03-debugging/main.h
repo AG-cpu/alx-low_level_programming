ce5265c12795c2615507ef5bbaccb3a8c8b89de9
AG-cpu <ahmadefatehy@gmail.com>
2022-04-13 09:50:17 -0700

    re13422

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int n);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
