3d02562e28c0d9882f1969942ccfb6acd262f8f7
AG-cpu <ahmadefatehy@gmail.com>
2022-04-14 11:56:07 -0700

    mo14422

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - two integers *.
+ * @a: The first int.
+ * @b: The second int.
+ * Return: multiplication re.
+ */
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
