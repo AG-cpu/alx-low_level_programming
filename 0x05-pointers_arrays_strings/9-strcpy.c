76ad47ccb9a9d5cb35c03cf96d4d30f166497456
AG-cpu <ahmadefatehy@gmail.com>
2022-04-18 11:28:50 -0700

    po18422

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  * @dest: A buffer to copy the string to.
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 
 {
 	int index = 0;
