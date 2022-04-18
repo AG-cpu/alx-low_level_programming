10cb0532cd6592ec9bff40ebb29cddf2e28caee5
AG-cpu <ahmadefatehy@gmail.com>
2022-04-18 10:57:50 -0700

    po18422

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *str)
+{
+	int len = 0, index;
+
+	while (str[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
