9fb9addec63cac0ddabfad50b09238ab27a7c9ba
AG-cpu <ahmadefatehy@gmail.com>
2022-04-29 13:14:16 -0700

    sttc29422

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*  _islower - print if alph is lower..etc
+* @c: checking proc..
+* Return: Always 0.
+*/
+
+int _islower(int c)
+
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+
+return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+
+
+/**
+ * _abs -  function that computes the absolute value of an integer.
+ * @n: The int checking proc..
+ * Return: int.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+{
+	return (n);
+}
+	else
+{
+	return (-n);
+}
+}
