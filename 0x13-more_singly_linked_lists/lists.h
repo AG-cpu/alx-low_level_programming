13b53dbba0a4734f30389f82bd50d572bbe17c73
AG-cpu <ahmadefatehy@gmail.com>
2022-05-24 04:56:28 -0700

    mr-linked-l24522

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,8 +5,8 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * : integer
+ * : points to the next node
  *
  * Description: singly linked list node structure
  *
@@ -21,7 +21,7 @@ typedef struct listint_s
 listint_t;
 
 int _putchar(char c);
-size_t print_listint(const listint_t2 *h);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
