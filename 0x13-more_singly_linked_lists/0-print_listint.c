708e66a7a109cd0543b3eebb724694fe3fc22a4a
AG-cpu <ahmadefatehy@gmail.com>
2022-05-24 02:38:19 -0700

    mr-linked-l24522

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,6 +4,7 @@
 /**
  * print_listint - listint_t list.
  * @h: the list head pointer.
+ * @n: newest elmnt
  * Return: The number of nodes to list.
  */
 size_t print_listint(const listint_t *h)
@@ -12,9 +13,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+	nodes++;
+	printf("%d\n", h->n);
+	h = h->next;
 	}
 
 	return (nodes);
